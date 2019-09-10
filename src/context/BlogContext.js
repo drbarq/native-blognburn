3a58a46bd25aa35cf14a7efc38ba42b23b86ae43
drbarq <J.Tustin@gmail.com>
2019-09-10 15:32:46 -0600

    adding in dispatch

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,21 +1,28 @@
-import React, {useState} from 'react'
+import React, {useReducer} from 'react'
 
 const BlogContext = React.createContext()
 
+const blogReducer = ( state, action ) => {
+    switch (action.type) {
+        case "add_blogpost":
+            return [
+                ...state, { title: `Blog Post #${state.length + 1}`}
+            ]
+        default:
+            return state
+    }
+}
+
 export const BlogProvider = ( props ) => {
-    const [blogPosts, setBlogPosts] = useState([])
+    const [blogPosts, dispatch] = useReducer(blogReducer, [{title: "Test Blog", content: "Test Content"}])
 
     const addBlogPost = () => {
-        return setBlogPosts([
-            ...blogPosts, 
-            {title: `new Post #${blogPosts.length + 1}`}
-        ])
+        dispatch({type: 'add_blogpost'})
     }
-
 
     return (
         <BlogContext.Provider
-            value={{data: blogPosts, addBlogPost}}
+            value={{data: blogPosts, addBlogPost }}
         >
             { props.children }
         </BlogContext.Provider>
